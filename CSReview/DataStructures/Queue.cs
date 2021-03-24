60978e00d82b7f6c0199c162f97394fc60db3525
Artem Prudnikov <art.prudnikov@gmail.com>
2021-03-24 19:14:16 +0300

    Implemented fixed size Queue

---
+++ CSReview/DataStructures/Queue.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CSReview.DataStructures
+{
+    public class Queue<T>
+    {
+        private T[] _queue;
+        private readonly int _size;
+
+        private int _head = 0;
+        private int _tail = 0;
+        private bool isFull = false;
+
+        public Queue(int size)
+        {
+            _size = size;
+            _queue = new T[_size];
+        }
+
+        public void Enqueue(T elem)
+        {
+            if (isFull) throw new OverflowException();
+
+            _queue[_tail] = elem;
+
+            if (_tail < _size - 1)
+            {
+                _tail++;
+            }
+            else
+            {
+                _tail = 0;
+                if (_head == 0) isFull = true;
+            }
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty()) throw new InvalidOperationException();
+
+            var toReturn = _queue[_head];
+            if (_head < _size - 1)
+            {
+                _head++;
+            }
+            else
+            {
+                _head = 0;
+            }
+
+            isFull = false;
+
+            return toReturn;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty()) throw new InvalidOperationException();
+
+            return _queue[_head];
+        }
+
+        public bool IsEmpty()
+        {
+            return _head == _tail;
+        }
+
+        public void Clear()
+        {
+            _head = 0;
+            _tail = 0;
+            isFull = false;
+        }
+    }
+
+    public class QueueTests
+    {
+        [Fact]
+        public void BordersTest()
+        {
+            var queue = new Queue<int>(3);
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            Assert.Throws<OverflowException>(() => queue.Enqueue(4));
+            
+            queue.Clear();
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+            Assert.Throws<InvalidOperationException>(() => queue.Peek());
+        }
+        
+        [Fact]
+        public void OperationsAreConsistent()
+        {
+            var arr = new[] {1, 2, 3, 4, 5};
+            var queue = new Queue<int>(10);
+            foreach (var el in arr)
+            {
+                queue.Enqueue(el);
+            }
+
+            foreach (var el in arr)
+            {
+                var saved = queue.Dequeue();
+                Assert.Equal(el, saved);
+            }
+            
+            Assert.True(queue.IsEmpty());
+        }
+    }
+}
